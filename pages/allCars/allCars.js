04d7e65aa97c3e376ebfa51943674bbb798245b0
Mojens <moha135h@stud.kea.dk>
2022-09-30 10:50:25 +0200

    first commit

---
+++ pages/allCars/allCars.js
@@ -0,0 +1,19 @@
+let CARS_URL = "http://localhost:8080/api/cars";
+
+let router;
+
+
+async function getAllCars() {
+    const allCars = await fetch(CARS_URL).then(r => r.json());
+    const rows = allCars.map(car => `<tr>
+    <td>${car.id}</td>
+    <td>${car.brand}</td>
+    <td>${car.model}</td>
+    <td>${car.pricePrDay}</td>
+  </tr>`).join("");
+  document.getElementById("tbl-body-cars").innerHTML = rows;
+}
+export function initAllCars(navigoRouter) {
+    getAllCars()
+    router = navigoRouter
+  }
